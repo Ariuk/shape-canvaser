83e72b25a14630dd960a72050103274f56c16fd2
ariuk <developer.ariuk@gmail.com>
2023-04-15 23:28:24 +0800

    func: getRectangularPositionAndSize, convertToObjectArray & getRectangularPositionAndSize

--- src/util.consts.js
+++ src/util.consts.js
@@ -1,5 +1,11 @@
 export const DRAW_TOOLS = ["Line", "Polygon", "Rectangle"];
 
+export const TOOL_NAMES = {
+  Line: "Line",
+  Polygon: "Polygon",
+  Rectangle: "Rectangle",
+};
+
 export const convertToArray = (arr) => {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -9,7 +15,7 @@ export const convertToArray = (arr) => {
   return result;
 };
 
-export const convertToObject = (arr) => {
+export const convertToObjectArray = (arr) => {
   const result = [];
   for (let i = 0; i < arr.length; i++) {
     const innerArr = arr[i];
@@ -17,4 +23,20 @@ export const convertToObject = (arr) => {
     result.push(obj);
   }
   return result;
+};
+
+export const getRectangularPositionAndSize = (points) => {
+  const RTOarray = convertToObjectArray(points);
+  const height = Math.abs(RTOarray[1].y - RTOarray[0].y);
+  const width = Math.abs(RTOarray[1].x - RTOarray[0].x);
+  const { x, y } = RTOarray[0];
+  return { x, y, width, height };
+};
+
+export const getPointsFromRectangularPositionAndSize = (data) => {
+  const { x, y, width, height } = data;
+  const point1 = { x: x, y: y };
+  const point2 = { x: x+width, y: y+height };
+  const points = [point1, point2];
+  return points;
 };
